d451f9b3ecc3628221a1a2b01d65e95a06bb252d
Cameron Finch <cameronfinch@Camerons-MacBook-Air.local>
2023-09-27 21:47:23 -0600

    commited

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -9,16 +9,18 @@ function GetYear(){
 
     var lastMod = document.lastModified;
     document.getElementById("lastModified").innerHTML = "Last Modified: " + lastMod;
-}
 
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
 
 
-hamButton.addEventListener('click', () => {
+    hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+}
+
+
 
 
 
