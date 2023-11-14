fb2561a876fa0810d66b1b3169675443e942cf80
Cameron Finch <cameronfinch@Camerons-MacBook-Air.local>
2023-11-14 07:43:22 -0700

    commited

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,25 +1,26 @@
+const url = '..//chamber/data/members.json';
+const cards = document.querySelector('.company-cards');
 async function getCompanies(){
-    const response = await fetch('https://github.com/CamFinch/wdd230/blob/main/chamber/data/members.json');
+    const response = await fetch(url);
     const data = await response.json();
-    console.table(data.companies);
-    const cards = document.querySelector('.company-cards');
-    const displayCompanies = (companies) => {
-        company.forEach((companies) => {
-            let cards = document.createElement('section');
-            let name = document.createElement('h3');
-            let address = document.createElement('p');
-            let phone = document.createElement('p');
-            let url = document.createElement('p');
-            let logo = document.createElement('img');
-            let member = document.createElement('p');
-            
+    console.log(data);
+   // const displayCompanies = (companies) => {
+       // company.forEach((companies) => {
+         //   let cards = document.createElement('section');
+         //   let name = document.createElement('h3');
+         //   let address = document.createElement('p');
+         //   let phone = document.createElement('p');
+         //   let url = document.createElement('p');
+         //   let logo = document.createElement('img');
+         //   let member = document.createElement('p');
             
 
             
-        });
-    }
+            
+       // });
+    //}
 
-    displayCompanies();
+   // displayCompanies();
 
 }
 
